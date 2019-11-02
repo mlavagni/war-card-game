48128080a4520a0659bc0a1d18c6447c6a18cac7
mark <mark@marks-MBP.lan1>
2019-11-01 22:32:15 -0500

    added create deck and shufflecards added son images and gif to the html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// import { readdir } from 'fs';
+
 let isGameOver = true;
 let isWar = false;
 
@@ -6,12 +8,13 @@ let arrayCardsTable = [];
 let cantPlayesr = 0;
 
 
+
 let player = {
     num: 1,
     score: 0,
-    cards: []
+    cards: [],
+    score: 0
 }
-
 
 
 
@@ -25,12 +28,39 @@ function findWinner (card1, card2){
    return (card1 > card2) ? card1 : card2
 }
 
-function shufleCards(arrayCards){
-    
-    return Math.floor(Math.random() * Math.floor(arrayCards.length));
-        
-}
+
 
 function flipUpCard(card){
 
 }
+
+function AddCarsdsToArray(){
+   // arrayCards
+}
+
+function createDeck() {
+   for (i= 2; i < 15; i++){
+       let cHeart = "../assets/card-deck-css/images/hearts";
+       let cClubs = "../assets/card-deck-css/images/clubs";
+       let cDiamonds = "../assets/card-deck-css/images/diamonds";
+       let cSpades = "../assets/card-deck-css/images/spades";
+        arrayCards.push({cHeart: i}, {cClubs: i}, {cDiamonds :i}, {cSpades: i});
+   }
+}
+
+function shufleCards(arrayCards){
+        var i = 0
+          , j = 0
+          , temp = null
+      
+        for (i = arrayCards.length - 1; i > 0; i -= 1) {
+          j = Math.floor(Math.random() * (i + 1))
+          temp = arrayCards[i]
+          arrayCards[i] = arrayCards[j]
+          arrayCards[j] = temp
+        }
+      }
+        
+
+
+createDeck()
